8d18b2c3caf5b29454f08d48794753bf1ef68171
Avinash Jagtap <avijagtap0493@gmail.com>
2026-02-17 17:01:11 +0530

    Add variables for AWS region, S3 bucket, and DynamoDB

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources into"
+  type        = string
+  default     = "us-east-2"
+}
+
+variable "s3_bucket_name" {
+  description = "S3 bucket name (must be globally unique)"
+  type        = string
+  default = "github-actions-terraform-state-bucket"
+}
+
+variable "dynamodb_table_name" {
+  description = "DynamoDB table name"
+  type        = string
+    default     = "github-actions-terraform-state-lock"
+}
