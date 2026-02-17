9baa5105b1b3090b10e1141b139fb80831874e62
Avinash Jagtap <avijagtap0493@gmail.com>
2026-02-17 16:59:50 +0530

    Create S3 bucket and DynamoDB table resources
    Added resources for S3 bucket and DynamoDB table with configurations.

---
+++ main.tf
@@ -0,0 +1,37 @@
+resource "aws_s3_bucket" "this" {
+  bucket = var.s3_bucket_name
+  acl    = "private"
+
+  force_destroy = true
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    ManagedBy = "Terraform"
+  }
+}
+
+resource "aws_dynamodb_table" "this" {
+  name         = var.dynamodb_table_name
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  tags = {
+    ManagedBy = "Terraform"
+  }
+}
